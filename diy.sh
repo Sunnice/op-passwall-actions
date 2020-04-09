6b6451d791f5d459634aa76270225255654dce4c
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-04-09 17:46:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 kenzok8_url='https://github.com/kenzok8/openwrt-packages.git'
 git clone $kenzok8_url package/kenzok8
+git clone https://github.com/Lienol/openwrt-package.git package/lienol
+git clone https://github.com/openwrt/luci.git package/openwrt
